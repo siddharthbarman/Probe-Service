28fcb5c5e416e403b90726e3ef70006ceb0f0275
Siddharth B <sbytestream@outlook.com>
2022-05-07 07:59:14 -0700

    Added ability to fail the service based on environment variables

--- probesvc.py
+++ probesvc.py
@@ -8,16 +8,6 @@ import logging
 import socket
 from cmdline import CmdLine
 
-# creating a Flask app
-app = Flask(__name__)
-started_at = datetime.now()
-cmd = CmdLine("-", sys.argv)
-version = "1"
- 
-@app.route('/health', methods=['GET'])
-def health():
-    return { "started_at": started_at, "uptime": (datetime.now() - started_at).total_seconds(), "version": version }
-
 def getEnvVarValue(name, defaultValue):
     value = os.getenv(name);
     if value == None:
@@ -28,34 +18,68 @@ def write_pid(file):
     with open(file, 'w') as f:
         f.write(str(os.getpid()))
 
+# creating a Flask app
+app = Flask(__name__)
+started_at = datetime.now()
+cmd = CmdLine("-", sys.argv)
+version = "1"
+
+pidfile = cmd.get_flag_value_default("PIDFILE", "probesvc.pid")
+logfile = cmd.get_flag_value_default("LOGFILE", "probesvc.log")    
+server_start_delay = int(getEnvVarValue("SERVER_START_DELAY", 0))
+server_ready_delay = int(getEnvVarValue("SERVER_READY_DELAY", 0))
+server_port = int(getEnvVarValue("SERVER_PORT", 5000))
+force_startup_failure = getEnvVarValue("FORCE_STARTUP_FAILURE", 0)
+force_ready_failure = getEnvVarValue("FORCE_READY_FAILURE", 0)
+fail_health_after_count = int(getEnvVarValue("FAIL_HEALTH_AFTER_COUNT", 0))
+health_call_count = 0
+
+@app.route('/health', methods=['GET'])
+def health():
+    global health_call_count
+    health_call_count = health_call_count + 1
+    if fail_health_after_count > 0 and health_call_count > fail_health_after_count:
+        return { "health_call_count":  health_call_count }, 500        
+    else:
+        return { 
+            "started_at": started_at, 
+            "uptime": (datetime.now() - started_at).total_seconds(), 
+            "version": version,
+            "call_count": health_call_count
+        }
+
 # driver function
 if __name__ == '__main__':
     for k, v in sorted(os.environ.items()):
         print(k, "=", v)
 
-    pidFile = cmd.get_flag_value_default("PIDFILE", "probesvc.pid")
-    logfile = cmd.get_flag_value_default("LOGFILE", "probesvc.log")    
-    serverStartDelay = getEnvVarValue("SERVER_START_DELAY", 0)
-    serverReadyDelay = getEnvVarValue("SERVER_READY_DELAY", 0)
-    serverPort = getEnvVarValue("SERVER_PORT", 5000)
-    
     print("IP:", socket.gethostbyname(socket.gethostname()))
-    print("Server port:", serverPort)
-    print("PidFile:", pidFile)
+    print("server_port:", server_port)
+    print("pidFile:", pidfile)
     print("logfile:", logfile)
-    print("serverStartDelay:", serverStartDelay)
-    print("serverReadyDelay", serverReadyDelay)
+    print("server_start_delay:", server_start_delay)
+    print("server_ready_delay:", server_ready_delay)
+    print("force_startup_failure:", force_startup_failure)
+    print("force_ready_failure:", force_ready_failure)
+    print("fail_health_after_count:", fail_health_after_count)
 
     logging.basicConfig(level=logging.INFO, handlers=[
         logging.FileHandler(logfile),
         logging.StreamHandler(sys.stdout)
     ])
 
-    sleep(serverStartDelay)
-    write_pid(pidFile)
+    sleep(server_start_delay)
+    if force_startup_failure == "1":
+        logging.critical("Forcing startup failure")
+        exit()
+
+    write_pid(pidfile)
     logging.info("PID file written")
 
-    sleep(serverReadyDelay)    
+    sleep(server_ready_delay)    
+    if force_ready_failure == "1":
+        logging.critical("Forcing ready failure")
+        exit()
     logging.info('Server started')
 
-    app.run(host ='0.0.0.0', port = serverPort, debug = False)
+    app.run(host ='0.0.0.0', port = server_port, debug = False)
--- readme.md
+++ readme.md
@@ -7,18 +7,20 @@ readiness and liveliness probes of Kubernetes.
 ### Configurations
 The service can be controller using the following environment variables:
 
-| Config Key   	        | Type   	    | Description   	| 
-|---	                |---	        |---	            |
-| SERVER_START_DELAY    | Env variable  | Seconds after which the service writes its pid to file|
-| SERVER_READY_DELAY    | Env variable  | Seconds after which HTTP server starts   	            |
-| PIDFILE               | Command line  | Full path to the pid file   	                        |
-| LOGFILE               | Command line  | Full path to the log file  	                        |
-
+| Config Key   	        | Type   	    | Description   	                                                            |    
+|---	                |---	        |---	                                                                        |
+| SERVER_START_DELAY    | Env variable  | Seconds after which the service writes its pid to file                        |
+| SERVER_READY_DELAY    | Env variable  | Seconds after which HTTP server starts   	                                    |
+| FORCE_STARTUP_FAILURE | Env variable  | Forces the program to terminate after executing the start delay if set to 1   |
+| FORCE_READY_FAILURE   | Env variable  | Forces the program to terminate after executing the ready delay if set to 1   |
+| PIDFILE               | Command line  | Full path to the pid file   	                                                |
+| LOGFILE               | Command line  | Full path to the log file                                                     |
 ### Endpoints
 The service has just one endpoint '/health' which returns the following 
 response:
 ```
 {
+    "call_count":2,
     "started_at":"Sat, 07 May 2022 15:58:14 GMT",
     "uptime":59.247903,
     "version":"1"
@@ -26,7 +28,7 @@ response:
 ```
 Calling the endpoint:
 ```
-$ curl -XGET http://localhost:5000/health
+$ curl -XGET -i http://localhost:5000/health
 ```
 
 ### Docker
@@ -37,11 +39,15 @@ $ docker build -t siddharthbarman/probesvc:1.0 .
 
 Run the container:
 ```
-docker run -n prober -p 5000:5000 -it siddharthbarman/probesvc:1.0
+$ docker run -n prober -p 5000:5000 -it siddharthbarman/probesvc:1.0
+```
+Run with environment variables:
+```
+$ docker run -e FAIL_HEALTH_AFTER_COUNT=2 -it siddharthbarman/probesvc:1.0
 ```
 
 Connect to the container:
 ```
-docker container exec -it prober bash
+$ docker container exec -it prober bash
 
 ```
